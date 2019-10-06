c2cc696e6ca5f5e0cc8878b12d86d532aa4bf94b
FemFX <ignedko34@gmail.com>
2019-10-06 09:15:43 +0300

    Components

---
+++ src/components/Game.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class>
+    <div class="center">
+      <span class="num1">{{ num1 }}</span> +
+      <span class="num2">{{ num2 }}</span> =
+      <input type="number" class="result" v-model="val" />
+    </div>
+    <button class="btn btn-primary next" @click="handleSubmit(val)">Next</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "Game",
+  props: ["num1", "num2"],
+  data() {
+    return {
+      val: ""
+    };
+  },
+  methods: {
+    handleSubmit(val) {
+      this.$emit("handleSubmit", val);
+      this.val = ""
+    }
+  }
+};
+</script>
+<style scoped>
+* {
+  color: #fff;
+}
+input {
+  color: #000;
+}
+.center {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  padding: 15px;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  font-size: 24px;
+  margin-left: 5px;
+}
+input {
+  width: 50px;
+}
+.next {
+  position: absolute;
+  bottom: 15px;
+  right: 15px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="score">Score : {{score}}</div>
+    <h1 class="text-center">MATH QUIZ</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: ["score"]
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+* {
+  color: #fff;
+  margin: 0;
+  padding: 0;
+}
+.score {
+  position: absolute;
+  top: 5px;
+  padding: 10px;
+}
+</style>
